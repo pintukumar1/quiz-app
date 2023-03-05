aa7b8df2fdc3874adcab305c39be123f537585f4
Pintu Kumar <pintu@opskube.com>
2023-03-05 12:05:18 +0530

    added ternary expression

--- lib/main.dart
+++ lib/main.dart
@@ -16,52 +16,58 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
 
-  void _answerQuestion() {
-    if(_questionIndex == 2) {
-      return;
+  final questions = const [
+    {
+      "questionText": 'What\'s your favourite color?',
+      "answers": ['Black', 'Red', 'Green', 'White']
+    },
+    {
+      "questionText": 'What\'s your favorite animal?',
+      "answers": ['Rabbit', 'Snake', 'Elephant', 'Lion']
+    },
+    {
+      "questionText": 'Who\'s your favourite instructor?',
+      "answers": ['Max', 'Manu', 'Pintu']
     }
+  ];
+
+  void _answerQuestion() {
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
     print(_questionIndex);
+    if (_questionIndex < questions.length) {
+      print('We have more questions.');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    var questions = [
-      {
-        "questionText": 'What\'s your favourite color?',
-        "answers": ['Black', 'Red', 'Green', 'White']
-      },
-      {
-        "questionText": 'What\'s your favorite animal?',
-        "answers": ['Rabbit', 'Snake', 'Elephant', 'Lion']
-      },
-      {
-        "questionText": 'Who\'s your favourite instructor?',
-        "answers": ['Max', 'Manu', 'Pintu']
-      }
-    ];
-
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My First App'),
         ),
-        body: Column(
-          children: [
-            Question(
-              questions[_questionIndex]['questionText'] as String,
-            ),
-            ...(questions[_questionIndex]['answers'] as List<String>)
-                .map((answer) {
-              return Answer(
-                _answerQuestion,
-                answer,
-              );
-            })
-          ],
-        ),
+        body: _questionIndex < questions.length
+            ? Column(
+                children: [
+                  Question(
+                    questions[_questionIndex]['questionText'] as String,
+                  ),
+                  ...(questions[_questionIndex]['answers'] as List<String>)
+                      .map((answer) {
+                    return Answer(
+                      _answerQuestion,
+                      answer,
+                    );
+                  })
+                ],
+              )
+            : Center(
+                child: Text(
+                  'Well done...',
+                ),
+              ),
       ),
     );
   }
