de6ec5d7d777b46a182d42ee98fe57d770e0c03a
Pintu Kumar <pintu@opskube.com>
2023-03-05 19:16:53 +0530

    Signed-off-by: Pintu Kumar <pintu@opskube.com>

--- lib/result.dart
+++ lib/result.dart
@@ -4,17 +4,28 @@ class Result extends StatelessWidget {
 
   final int totalScore;
 
+  Result(this.totalScore);
+
   String get resultPhrase {
     String resultText;
     if(totalScore <=8) {
-      resultText = 
+      resultText = 'Your are awesome and innocent';
+    }else if(totalScore<=12) {
+      resultText = 'Pretty likeable!';
+    }else if(totalScore<=16){
+      resultText = 'You are strange.. >?';
+    }else {
+      resultText = 'You are so bad.';
     }
+    return resultText;
   }
+
   @override
   Widget build(BuildContext context) {
-    return const Center(
+    return Center(
           child: Text(
-            'Well done...',
+            resultPhrase,
+            style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold,),
           ),
         );
   }
